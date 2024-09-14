ef76ea7e73082622df919e6db8bf9c71a3475624
JaraxW <1771394286@qq.com>
2024-09-14 20:45:21 +0800

    Update A8chat.js

--- A8chat.js
+++ A8chat.js
@@ -26,41 +26,6 @@ hostname = *.gameloft.com,ads.vungle.com,*.unity3d.com,*.applovin.com, web.faceb
 let obj = {};
 let res = JSON.parse(typeof $response !== "undefined" && $response.body || null);
 
-// 广告相关的处理逻辑
-const u3d_ad = /config.json/;
-if (u3d_ad.test($request.url)) {
-    let body = res;
-    if (body["SRR"]) {
-        body["SRR"]["placements"].forEach(ad_item => {
-            Object.assign(ad_item, {
-                allowSkip: true,
-                closeTimerDuration: 1,
-                skipInSeconds: 1,
-                adFormat: "interstitial",
-                disableBackButton: false,
-                optOutEnabled: true,
-                experimentation: { admobMednLoadTimeoutInSec: "1" },
-                isSkipToAppSheetEnabled: false,
-                assetCaching: "voluntary",
-                banner: { refreshRate: 5 },
-                enabled: false
-            });
-        });
-        obj.body = JSON.stringify(body);
-        $done(obj);
-    }
-}
-
-// Facebook广告相关逻辑
-const adnw = /facebook.com\/adnw_sync2/;
-if (adnw.test($request.url)) {
-    let body = res;
-    body["refresh"]["target_refresh_s"] = 10;
-    body["bundles"]["feature_config"]["data"]["feature_config"]["adnw_android_network_default_connection_timeout_ms"] = 100;
-    obj.body = JSON.stringify(body);
-    $done(obj);
-}
-
 // 解锁内购和解锁全部车辆的逻辑
 const me = /gameloft.com\/configs\/users\/me/;
 if (me.test($request.url)) {
@@ -91,80 +56,9 @@ if (me.test($request.url)) {
     $done(obj);
 }
 
-// 个人资料同步的处理
-const myprofile = /gameloft.com\/profiles\/me\/myprofile/;
-if (myprofile.test($request.url)) {
-    let body = res;
-    delete body["_infractions"];
-    if (body["_adjoe_reward"]) {
-        body["_adjoe_reward"]["data"] = "";
-        body["_ad_rewards"]["data"] = "";
-        body["_ads_progressive"] = {};
-        body["_Vip"]["level"] = 15;
-        body["_Vip"]["initial_points"] = 155;
-    }
-    obj.body = JSON.stringify(body);
-    $done(obj);
-}
-
-// 恢复购买的处理
-const restore = /inapp_crm\/index.php/;
-if (restore.test($request.url)) {
-    if (!/action/.test($request.url)) {
-        let body = JSON.stringify([
-            {
-                "status": "delivered",
-                "id": "Car_Bundle_350_iinm",
-                "info": [{ "quantity": 1, "item": "Nissan_Leaf_Nismo_RC___CAR_PRICE" }],
-                "transaction_id": "310156474458",
-                "subscription": true,
-                "item_id": "com.gameloft.asphalt8.iOS_car_bundle_350"
-            },
-            {
-                "status": "delivered",
-                "id": "Car_Bundle_356_s6pe",
-                "info": [{ "quantity": 1, "item": "Ariel_Atom_V8___CAR_PRICE" }],
-                "transaction_id": "310156424684",
-                "subscription": true,
-                "item_id": "com.gameloft.asphalt8.iOS_car_bundle_356"
-            }
-        ]);
-        $done({ body });
-    }
-}
-
-// 授权请求的处理
-const authorize = /^https:([\S\s]*?)gameloft.com\/authorize/;
-if (authorize.test($request.url)) {
-    let body = $request.body;
-    body = body.replace(/username([\S\s]+?)[\&]/, "username=anonymous%2FOtMyt5EPkvgRcxM%3AdjNjQ3MjEwM1fMT%dr2BkYZ71D&");
-    body = body.replace(/password([\S\s]+?)[\&]/, "password=GIHI7x9ofH5q55vJ&");
-    $done({ body });
-}
-
-// 比赛预处理逻辑
-const pre_tle_race = /^https:([\S\s]*?)energy\/pre_tle_race.php/;
-if (pre_tle_race.test($request.url)) {
-    if (res && res["body"]) {
-        let body = res;
-        let timestamp = Math.floor((new Date().getTime() + 1000 * 60 * 60 * 24 * 364) / 1000);
-
-        body["body"]["infractions_sync"]["body"]["infractions"] = "";
-        body["body"]["boosters_sync"]["body"]["active"] = {
-            "extra_tank": { "min": timestamp },
-            "performance": { "min": timestamp },
-            "nitro": { "min": timestamp },
-            "credits": { "min": timestamp }
-        };
-        obj.body = JSON.stringify(body);
-    }
-    $done(obj);
-}
-
-// 同步请求的处理和车辆升级
+// 车辆升级逻辑
 const sync = /^https:([\S\s]*?)sync_all.php/;
-const script_g = /^https:([\S\s]*?)gameloft.com\/scripts([\S\s]*?).php/;
-if (sync.test($request.url) || script_g.test($request.url)) {
+if (sync.test($request.url)) {
     if (res && res["body"]) {
         let body = res;
 
@@ -187,16 +81,6 @@ if (sync.test($request.url) || script_g.test($request.url)) {
                 };
             }
         }
-
-        // 同步奖励信息
-        body["body"]["boosters_sync"]["body"]["active"] = {
-            "extra_tank": { "min": 1712288961 },
-            "performance": { "min": 1712288961 },
-            "nitro": { "min": 1712288961 },
-            "credits": { "min": 1712288961 }
-        };
-
-        body["body"]["vip_full_sync"]["body"]["level"] = 15;
 
         obj.body = JSON.stringify(body);
         $done(obj);
