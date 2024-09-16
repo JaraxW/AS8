96ea80bdd5c09fb3ca169dc35367e6a85a97d465
JaraxW <1771394286@qq.com>
2024-09-17 00:27:37 +0800

    Update A8unlock.js

--- A8unlock.js
+++ A8unlock.js
@@ -110,35 +110,7 @@ if (myprofile.test($request.url)) {
     $done(obj);
 }
 
-const restore = /inapp_crm\/index.php/;
-if (restore.test($request.url)) {
-    if (!/action/.test($request.url)) {
-        let obj = [
-            {
-                "status": "delivered",
-                "id": "Car_Bundle_350_iinm",
-                "info": [
-                    { "quantity": 1, "item": "Nissan_Leaf_Nismo_RC___CAR_PRICE" }
-                ],
-                "transaction_id": "310156474458",
-                "subscription": true,
-                "item_id": "com.gameloft.asphalt8.iOS_car_bundle_350"
-            },
-            {
-                "status": "delivered",
-                "id": "Car_Bundle_356_s6pe",
-                "info": [
-                    { "quantity": 1, "item": "Ariel_Atom_V8___CAR_PRICE" }
-                ],
-                "transaction_id": "310156424684",
-                "subscription": true,
-                "item_id": "com.gameloft.asphalt8.iOS_car_bundle_356"
-            }
-        ];
-        let body = JSON.stringify(obj);
-        $done({ body });
-    }
-}
+
 
 const authorize = /^https:([\S\s]*?)gameloft.com\/authorize/;
 if (authorize.test($request.url)) {
@@ -198,9 +170,9 @@ if (sync.test($request.url) || script_g.test($request.url)) {
             body["body"]["progressive_ads_sync"]["body"]["duration"] = 372800;
         }
 
-        //if (body["body"]["server_items_full_sync"]) {
-        //    body["body"]["server_items_full_sync"]["body"]["cars"] = cars;
-       // }
+        if (body["body"]["server_items_full_sync"]) {
+            body["body"]["server_items_full_sync"]["body"]["cars"] = cars;
+        }
 
         body["body"]["prokits_car_parts_full_sync"] = {
             "body": {
